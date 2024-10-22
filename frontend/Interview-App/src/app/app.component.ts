074e1d1b5dc34f68e4f604fb857d43fb5b7dbed2
chris <chriscastor@neuroflow.com>
2024-10-22 12:09:54 -0400

    Fix toolbar height once view is set

--- frontend/Interview-App/src/app/app.component.ts
+++ frontend/Interview-App/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('toolbar') toolbar!: ElementRef;
   title = 'Interview-App';
+
+  ngAfterViewChecked() {
+    setTimeout(() => {
+      this.toolbar.nativeElement.style.minHeight = 100;
+    }, 50);
+  }
 }
